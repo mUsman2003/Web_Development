d08bdb2b716bc61a7b2df795b9809aabad325c8d
usman <usman2003.fb@gmail.com>
2024-08-02 02:51:04 -0700

    mods

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-console.log("Hello")
-
-var slugify = require('slugify')
-
-let a=slugify('some string') // some-string
-
-// if you prefer something other than '-' as separator
-console.log(a)
-let b=slugify('some string', '_')  // some_string
-console.log(b)
+const { createServer } = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
